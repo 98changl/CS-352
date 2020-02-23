e4c75f48eb9966903e11e18568461e4afa9a3974
98changl <98changl@gmail.com>
2020-02-23 15:22:25 -0500

    Update client.py

--- project1/client.py
+++ project1/client.py
@@ -3,8 +3,10 @@ import time
 import random
 
 import socket
+import sys
 
-def client():
+def client(rsHostname, rsListenPort, tsListenPort):
+    # connect to rs server
     try:
         cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("[C]: Client socket created")
@@ -23,12 +25,40 @@ def client():
     # Receive data from the server
     data_from_server=cs.recv(100)
     print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
-
+    
+    
+    # if rs returns failer, connect to ts server
+    try:
+        ts = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[C]: Client socket created")
+    except socket.error as err:
+        print('socket open error: {} \n'.format(err))
+        exit()
+        
+    
     # close the client socket
     cs.close()
     exit()
+    
+def main():
+    if len(sys.argv) < 3:
+        print('not enough arguments error: {} \n'.format(err))
+        exit()
+    
+    # read in arguments from command
+    args = str(sys.argv)
+    print args
+    
+    rsHostname = args[0]
+    rsListenPort = args[1]
+    tsListenPort = args[2]
+    del args
+    
+    client(rsHostname, rsListenPort, tsListenPort)
+    exit()
 
 if __name__ == "__main__":
+    main()
     t1 = threading.Thread(name='server', target=server)
     t1.start()
 
