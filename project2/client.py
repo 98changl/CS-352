dacb1da80420da2e48fc75527077782436e8bc7f
Manpreet Singh <manpreetsingh586@gmail.com>
2020-03-26 18:31:07 -0400

    worked on ts1,ts2,client

--- project2/client.py
+++ project2/client.py
@@ -1,1 +1,89 @@
+import threading
+import time
+import random
 
+import socket
+import sys
+
+def client(table, lsHostname, lsListenPort):
+
+    resolve = open("RESOLVED.txt", "w")
+    for name in table:
+
+        try:
+            ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print("[C]: Client socket created")
+        except socket.error as err:
+            print('socket open error: {} \n'.format(err))
+            exit()
+        
+        # Define the port on which you want to connect to the server
+        # port = 50007
+        localhost_addr = socket.gethostbyname(socket.gethostname())
+
+        # connect to the server on local machine
+        ls_server_binding = (localhost_addr, lsListenPort)
+        #print(rs_server_binding)
+        ls.connect(ls_server_binding)
+    
+    
+        # send data to the server
+        ls.send( name )
+
+        # Receive data from the server
+        data_from_server = ls.recv(1000)
+        print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+
+        # open text file to write
+        #resolve = open("RESOLVED.txt", "w")
+
+        #data = data_from_server.split()
+        #hostname = data[0]
+        #address = data[1]
+        #flag = data[2]
+        resolve.write(data_from_server)
+        
+    resolve.close()
+        #exit()
+    
+def main():
+    if len(sys.argv) < 3:
+        print('not enough arguments error: {} \n'.format(err))
+        exit()
+    
+    
+    # convert arguments into strings to read into client
+    lsHostname = str(sys.argv[1])
+    lsListenPort = int(sys.argv[2])
+    
+    
+    
+    # read in file
+    f = open("ROJ2-HNS.txt", "r")
+    
+    # set up list of hostnames 
+    table = []
+    read = f.readlines()
+    
+    # populate the list with hostnames
+    for line in read:
+        # check to see if the line is valid
+        #print( line )
+        
+        # add the hostname into the list
+        table.append( line )
+    
+    client(table, lsHostname, lsListenPort)
+    exit()
+
+if __name__ == "__main__":
+    main()
+    #t1 = threading.Thread(name='server', target=server)
+    #t1.start()
+
+    #time.sleep(random.random() * 5)
+    #t2 = threading.Thread(name='client', target=client)
+    #t2.start()
+
+    #time.sleep(5)
+    print("Done.")
--- project2/ts2.py
+++ project2/ts2.py
@@ -1,1 +1,117 @@
 
+import threading
+import time
+import random
+
+import socket
+import sys
+
+# object to store three variables
+class DNSnode:
+    def __init__(self, hostname, address, flag):
+        self.hostname = hostname
+        self.address = address 
+        self.flag = flag
+
+def search(table, hostname):
+    for node in table:
+        if node.hostname == hostname:
+            return node
+    return -1
+
+def server(table, ts2ListenPort):
+    try:
+        ts2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[S]: Server socket created")
+    except socket.error as err:
+        print('socket open error: {}\n'.format(err))
+        exit()
+
+    #server_binding = ("localhost", tsListenPort)
+    ts2.bind( ('', ts2ListenPort) )
+    print("[S]: Server bind created")
+    
+    ts2.listen(1)
+    host = socket.gethostname()
+    print("[S]: Server host name is {}".format(host))
+    localhost_ip = (socket.gethostbyname(host))
+    print("[S]: Server IP address is {}".format(localhost_ip))
+
+
+    
+    while True:
+        # connect with client
+        csockid, addr = ts2.accept()
+        print ("[S]: Got a connection request from a client at {}".format(addr))
+    
+        # recieve client input
+        data_from_client = csockid.recv(100)
+    
+        # translate client message into DNS table format
+        hostname = data_from_client
+
+        # search for hostname
+        result = search(table, hostname)
+    
+        # reply to client
+        if result == -1:
+            # send error message to client
+            break
+        else:
+            # send node data as a string to client
+            msg = result.hostname + " " + result.address + " " + result.flag
+            print(msg)
+            csockid.send(msg.encode('utf-8'))
+    
+    # close the server socket
+    ts2.close()
+    exit()
+
+def main():
+    if len(sys.argv) < 1:
+        print('not enough arguments error: {} \n'.format(err))
+        exit()
+    
+    # read in arguments from command
+    arg = sys.argv[1]
+    tsListenPort = int( arg )
+    
+    
+    # read in file
+    f = open("PROJ2-DNSTS2.txt", "r")
+    
+    # set up DNS table
+    table = []
+    read = f.readlines()
+    
+    # populate the DNS table with values
+    for line in read:
+        # splits the read in string into an array of its elements
+        inputs = line.split()
+        
+        # check to see if the split is valid
+        #print(inputs)
+        
+        # set up variables
+        hostname = inputs[0]
+        address = inputs[1]
+        flag = inputs[2]
+        
+        # add the variables into the DNS table 
+        table.append( DNSnode(hostname, address, flag) )
+    
+    # run the server
+    server(table, ts1ListenPort)
+    exit()
+    
+if __name__ == "__main__":
+    main()
+    #t1 = threading.Thread(name='server', target=server)
+    #t1.start()
+
+    #time.sleep(random.random() * 5)
+    #t2 = threading.Thread(name='client', target=client)
+    #t2.start()
+
+    #time.sleep(5)
+    print("Done.")
